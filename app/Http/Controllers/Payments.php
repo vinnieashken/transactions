107de48bda0542839cec0d49ccb6fcaabe402bef
caydee <caydee2010@aol.com>
2020-03-21 17:33:24 +0300

    shortcode

--- app/Http/Controllers/Payments.php
+++ app/Http/Controllers/Payments.php
@@ -50,8 +50,8 @@ class Payments extends Controller
             }
         public function editshortcode(Request $request)
             {
-
-                $shortcode                  =   Shortcode::find($request->id);
+                $shortcode                  =   new Shortcode();
+                $shortcode->find($request->id);
                 $shortcode->shortcode       =   $request->shortcode;
                 $shortcode->shortcode_type  =   $request->type;
                 $shortcode->consumerkey     =   trim($request->consumerkey);
