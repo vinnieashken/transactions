d9641869041e359cfc1d955c37eaa21ef5aec294
Dennis Kiptoo Kiptugen <dennis.kiptoo@caydeesoft.org>
2020-05-27 06:57:28 +0300

    Transactions

--- resources/views/admin/includes/js.blade.php
+++ resources/views/admin/includes/js.blade.php
@@ -120,7 +120,7 @@
             ],
             "footerCallback": function ( row, data, start, end, display) {
             var api = this.api(), data;
-            console.log(ajax.params());
+            console.log(data);
             // Remove the formatting to get integer data for summation
             // Update footer
             $( api.column( 8 ).footer() ).html(
