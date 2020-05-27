97b4538a217918c6dba83ec13ea9992b8ee87e29
Dennis Kiptoo Kiptugen <dennis.kiptoo@caydeesoft.org>
2020-05-27 06:09:11 +0300

    Transactions

--- resources/views/admin/includes/js.blade.php
+++ resources/views/admin/includes/js.blade.php
@@ -121,7 +121,7 @@
             ],
             "footerCallback": function( tfoot, data, start, end, display ) {
                 var api = this.api();
-                console.log(api);
+                console.log( api.column( 9 ));
             }
 
         });
