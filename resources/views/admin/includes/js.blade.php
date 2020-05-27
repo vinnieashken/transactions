857f0b7277966bafa842986bf87086e5325fb9c6
Dennis Kiptoo Kiptugen <dennis.kiptoo@caydeesoft.org>
2020-05-27 06:21:12 +0300

    Transactions

--- resources/views/admin/includes/js.blade.php
+++ resources/views/admin/includes/js.blade.php
@@ -113,20 +113,46 @@
                 { "data": "msisdn" },
                 { "data": "transaction_code" },
                 { "data": "account" },
-
                 { "data": "origin" },
                 { "data": "channel" },
                 { "data": "transaction_time" },
                 { "data": "amount" }
             ],
-            "footerCallback": function( tfoot, data, start, end, display ) {
-                var api = this.api();
-                $( api.column( 9 ).footer()).html(
-                    api.column( 9 ).data().reduce( function ( a, b ) {
-                        return a + b;
-                    }, 0 )
-                );
-            }
+            ,
+
+            "footerCallback": function ( row, data, start, end, display ) {
+            var api = this.api(), data;
+
+            // Remove the formatting to get integer data for summation
+            var intVal = function ( i ) {
+                return typeof i === 'string' ?
+                    i.replace(/[\$,]/g, '')*1 :
+                    typeof i === 'number' ?
+                        i : 0;
+            };
+
+            // Total over all pages
+            total = api
+                .column( 9 )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Total over this page
+            pageTotal = api
+                .column( 9, { page: 'current'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Update footer
+            $( api.column( 9 ).footer() ).html(
+                '$'+pageTotal +' ( $'+ total +' total)'
+            );
+        },
+
 
         });
     });
