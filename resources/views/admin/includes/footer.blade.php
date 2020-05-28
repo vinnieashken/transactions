e9ac1ed393142157b0e690f1434413952d7c8f5e
Dennis Kiptoo Kiptugen <dennis.kiptoo@caydeesoft.org>
2020-05-28 08:44:44 +0300

    Dashboard

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -4,24 +4,11 @@
         <div class="row text-muted">
             <div class="col-6 text-left">
                 <p class="mb-0">
-                    &copy; <a href="index-2.html" class="text-muted">Paper</a>
+                    &copy; <a href="index-2.html" class="text-muted">Mpesa</a>
                 </p>
             </div>
             <div class="col-6 text-right">
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a class="text-muted" href="#">About us</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="text-muted" href="#">Help</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="text-muted" href="#">Contact</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="text-muted" href="#">Terms & Conditions</a>
-                    </li>
-                </ul>
+
             </div>
         </div>
     </div>
