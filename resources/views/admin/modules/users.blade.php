bffd9234c7ea5b5d00c8563acb6805c6a74808ec
Dennis Kiptoo Kiptugen <dennis.kiptoo@caydeesoft.org>
2020-05-29 07:21:23 +0300

    user mamanegment

--- resources/views/admin/modules/users.blade.php
+++ resources/views/admin/modules/users.blade.php
@@ -2,5 +2,35 @@
 @section('title', 'User Management')
 @section('subtitle','User Management')
 @section('content')
+    <div class="card">
+        <div class="card-header">
+            <div class="text-right"><button class="btn btn-default" data-toggle="modal" data-target="#addModal">
+                    <i class="align-middle" data-feather="plus"></i> Add Shortcode</button>
+            </div>
+        </div>
+        <div class="card-body">
+            <table id="users-table" class="table table-striped table-hover custom-list-table">
+                <thead>
+                <tr>
+                    <th>*</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
 
+                <tfoot>
+                <tr>
+                    <th>*</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+                </tfoot>
+            </table>
+
+        </div>
+    </div>
 @endsection
