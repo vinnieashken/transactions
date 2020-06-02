ca23f6f3ed646300fee9514e1dedc0d49802e3ed
Dennis Kiptoo Kiptugen <dennis.kiptoo@caydeesoft.org>
2020-06-02 14:20:59 +0300

    User roles

--- resources/views/admin/modules/users.blade.php
+++ resources/views/admin/modules/users.blade.php
@@ -35,4 +35,47 @@
 
         </div>
     </div>
+    <div class="modal" tabindex="-1" role="dialog" id="addModal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="" class="form form-horizontal">
+                        <div class="form-group">
+                            <label for="name" class="control-label">Name</label>
+                            <input type="text" name="fullname" id="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="control-label">Email</label>
+                            <input type="email" name="email" id="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="roles" class="control-label">Roles</label>
+                            <div class="form-group">
+                                <label for="users" class="control-label">Users</label>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                    <label class="form-check-label" for="inlineCheckbox1">Add</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                                    <label class="form-check-label" for="inlineCheckbox2">Update</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group form-row">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Invite Member</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
