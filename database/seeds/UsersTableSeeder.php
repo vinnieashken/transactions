f4c2eec09d8d9b73e4eaa5f89869ffaf4ae5cf6c
Dennis Kiptoo Kiptugen <dennis.kiptoo@programmer.net>
2020-06-26 14:53:02 +0300

    users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,12 +11,37 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
         $id = DB::table('users')->insertGetId([
-            'name'      =>  'Dennis Kiptoo Kiptugen',
-            'username'  =>  'caydee',
-            'email'     =>  'caydee209@gmail.com',
-            'password'  =>  bcrypt('secret'),
+            'name'      =>  'Jared Kidambi',
+            'username'  =>  'jkidambi',
+            'email'     =>  'jkidambi@standardmedia.co.ke',
+            'password'  =>  bcrypt('RK147nfnBH'),
+            'status'    =>  true,
+
+        ]);
+        DB::table('user_roles')->insert([
+            'user_id'       =>  $id,
+            'access_name'   =>  'users',
+            'access_value'  =>  serialize(['add','update','delete'])
+        ]);
+        $id = DB::table('users')->insertGetId([
+            'name'      =>  'Patrick Onganga',
+            'username'  =>  'pogangah',
+            'email'     =>  'pogangah@STANDARDMEDIA.CO.KE',
+            'password'  =>  bcrypt('nMbv0UQa3L'),
+            'status'    =>  true,
+
+        ]);
+        DB::table('user_roles')->insert([
+            'user_id'       =>  $id,
+            'access_name'   =>  'users',
+            'access_value'  =>  serialize(['add','update','delete'])
+        ]);
+        $id = DB::table('users')->insertGetId([
+            'name'      =>  'Matthew Shahi',
+            'username'  =>  'mshahi',
+            'email'     =>  'mshahi@STANDARDMEDIA.CO.KE',
+            'password'  =>  bcrypt('PhkLTOHBGC'),
             'status'    =>  true,
 
         ]);
